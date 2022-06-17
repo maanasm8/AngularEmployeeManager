f3f8cace43af6be120183346ec3cc5489241bcb2
maanasm8 <maanasm8@vt.edu>
2022-06-17 14:06:35 -0400

    syntax error fixed
    fixed synatx error

--- AngularCRUD/src/app/app.component.ts
+++ AngularCRUD/src/app/app.component.ts
@@ -1,22 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from './services/api.service';
-import 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   constructor(private employeeS: ApiService) {}
   title = 'AngularCRUD';
- ngOnInit():void{
-  
- }
+  ngOnInit(): void {}
   getEmployeeData() {
     this.employeeS.getEmployee().subscribe({
-      next: (res)=>{
+      next: (res) => {
         console.log(res);
-      }
+      },
     });
   }
 }
